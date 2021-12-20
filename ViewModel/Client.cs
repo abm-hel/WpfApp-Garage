723d5190434d0e658eac8156a4f74b3f15168714
Adil BEN MOUSSA <adil-benmoussa@hotmail.fr>
2021-12-20 11:56:06 +0100

    Ajout de la fenêtre Clients

---
+++ ViewModel/Client.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp_Garage;
+using System.Collections.ObjectModel;
+
+
+namespace WpfApp_Garage.ViewModel
+{
+    class Client
+    {
+    }
+}
