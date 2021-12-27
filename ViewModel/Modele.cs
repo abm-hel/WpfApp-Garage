7b65ea6b6dbf4bd620e9dbba3e9c834bd799ff95
Adil BEN MOUSSA <adil-benmoussa@hotmail.fr>
2021-12-27 15:38:32 +0100

    Développement du model view 'Modele' - création des méthodes

--- ViewModel/Modele.cs
+++ ViewModel/Modele.cs
@@ -50,13 +50,111 @@ namespace WpfApp_Garage.ViewModel
             get { return _unModele; }
             set { AssignerChamp<VM_UnModele>(ref _unModele, value, System.Reflection.MethodBase.GetCurrentMethod().Name); }
         }
-        private ObservableCollection<C_Client> _bcpModeles = new ObservableCollection<C_Client>();
-        public ObservableCollection<C_Client> bcpModeles
+        private ObservableCollection<C_Modele> _bcpModeles = new ObservableCollection<C_Modele>();
+        public ObservableCollection<C_Modele> bcpModeles
         {
             get { return _bcpModeles; }
             set { _bcpModeles = value; }
         }
         #endregion
+
+        #region Commandes
+        public BaseCommande commandeConfirmer { get; set; }
+        public BaseCommande commandeAnnuler { get; set; }
+        public BaseCommande commandeAjouter { get; set; }
+        public BaseCommande commandeModifier { get; set; }
+        public BaseCommande commandeSupprimer { get; set; }
+        // public BaseCommande commandeEssaiSelMult { get; set; }
+        #endregion
+
+        public VM_Modele()
+        {
+            unModele = new VM_UnModele();
+
+            bcpModeles = ChargerModeles(chaineConnexion);
+            ActiverUneFiche = false;
+            commandeConfirmer = new BaseCommande(Confirmer);
+            commandeAnnuler = new BaseCommande(Annuler);
+            commandeAjouter = new BaseCommande(Ajouter);
+            commandeModifier = new BaseCommande(Modifier);
+            commandeSupprimer = new BaseCommande(Supprimer);
+            //commandeEssaiSelMult = new BaseCommande(EssaiSelMult);
+        }
+
+        public void Confirmer()
+        {
+            if (nAjout == -1)
+            {
+                unModele.id = new G_Modele(chaineConnexion).Ajouter(unModele.modele, unModele.motorisation, unModele.carburant, unModele.cylindree, unModele.puissance, unModele.consommation, unModele.poids);
+                bcpModeles.Add(new C_Modele(unModele.modele, unModele.motorisation, unModele.carburant, unModele.cylindree, unModele.puissance, unModele.consommation, unModele.poids));
+            }
+            else
+            {
+                new G_Modele(chaineConnexion).Modifier(unModele.id, unModele.modele, unModele.motorisation, unModele.carburant, unModele.cylindree, unModele.puissance, unModele.consommation, unModele.poids);
+                bcpModeles[nAjout] = new C_Modele(unModele.id, unModele.modele, unModele.motorisation, unModele.carburant, unModele.cylindree, unModele.puissance, unModele.consommation, unModele.poids);
+            }
+            ActiverUneFiche = false;
+        }
+
+        public void Annuler()
+        {
+            ActiverUneFiche = false;
+        }
+
+        public void Ajouter()
+        {
+            unModele = new VM_UnModele();
+            nAjout = -1;
+            ActiverUneFiche = true;
+        }
+
+        public void Modifier()
+        {
+            if (modeleSelectionnee != null)
+            {
+                C_Modele Tmp = new G_Modele(chaineConnexion).Lire_ID(unModele.id);
+                unModele = new VM_UnModele();
+                unModele.id = Tmp.id;
+                unModele.modele = Tmp.modele;
+                unModele.motorisation = Tmp.motorisation;
+                unModele.carburant = Tmp.carburant;
+                unModele.cylindree = (int)Tmp.cylindree;
+                unModele.puissance = (int)Tmp.puissance;
+                unModele.consommation = (float)Tmp.consommation;
+                unModele.poids = (int)Tmp.poids;
+                nAjout = bcpModeles.IndexOf(modeleSelectionnee);
+                ActiverUneFiche = true;
+            }
+        }
+        public void Supprimer()
+        {
+            if (modeleSelectionnee != null)
+            {
+                new G_Client(chaineConnexion).Supprimer(modeleSelectionnee.id);
+                bcpModeles.Remove(modeleSelectionnee);
+            }
+        }
+
+        private ObservableCollection<C_Modele> ChargerModeles(string chaineConnexion)
+        {
+            ObservableCollection<C_Modele> rep = new ObservableCollection<C_Modele>();
+            List<C_Modele> lTmp = new G_Modele(chaineConnexion).Lire("id");
+            foreach (C_Modele Tmp in lTmp)
+                rep.Add(Tmp);
+            return rep;
+        }
+
+        public void ModeleSelectionnee2UnModele()
+        {
+            unModele.id = modeleSelectionnee.id;
+            unModele.modele = modeleSelectionnee.modele;
+            unModele.motorisation = modeleSelectionnee.motorisation;
+            unModele.carburant = modeleSelectionnee.carburant;
+            unModele.cylindree = (int)modeleSelectionnee.cylindree;
+            unModele.puissance = (int)modeleSelectionnee.puissance;
+            unModele.consommation = (float)modeleSelectionnee.consommation;
+            unModele.poids = (int) modeleSelectionnee.poids;
+        }
 
         public class VM_UnModele : BasePropriete
         {
