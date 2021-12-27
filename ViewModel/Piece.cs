9cb721d15191e751452b2c9df6b007621d633978
Adil BEN MOUSSA <adil-benmoussa@hotmail.fr>
2021-12-27 17:09:21 +0100

    Création de View model pour les pièces

---
+++ ViewModel/Piece.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Projet_Garage.Classes;
+using Projet_Garage.Gestion;
+using System.Configuration;
+using System.Collections.ObjectModel;
+
+namespace WpfApp_Garage.ViewModel
+{
+    class VM_Piece : BasePropriete
+    {
+    }
+}
