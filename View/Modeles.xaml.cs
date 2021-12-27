90badc57ab0c5fc2a9aa5fdd4f8db0d8486ba5fa
Adil BEN MOUSSA <adil-benmoussa@hotmail.fr>
2021-12-27 11:02:55 +0100

    Création et conception de la fenêtre qui permet de gérer les momodèles

--- View/Modeles.xaml.cs
+++ View/Modeles.xaml.cs
@@ -23,5 +23,10 @@ namespace WpfApp_Garage.View
         {
             InitializeComponent();
         }
+
+        private void dataGridModeles_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
