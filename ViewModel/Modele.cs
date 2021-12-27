c2411efe20762b03c99ebb4b5b6dcadf2a9bc795
Adil BEN MOUSSA <adil-benmoussa@hotmail.fr>
2021-12-27 11:16:46 +0100

    Création de la classe VM_UnModele

--- ViewModel/Modele.cs
+++ ViewModel/Modele.cs
@@ -42,5 +42,73 @@ namespace WpfApp_Garage.ViewModel
             set { AssignerChamp<C_Modele>(ref _modeleSelectionnee, value, System.Reflection.MethodBase.GetCurrentMethod().Name); }
         }
         #endregion
+
+        #region Données extérieures
+        private VM_UnModele _unModele;
+        public VM_UnModele unModele
+        {
+            get { return _unModele; }
+            set { AssignerChamp<VM_UnModele>(ref _unModele, value, System.Reflection.MethodBase.GetCurrentMethod().Name); }
+        }
+        private ObservableCollection<C_Client> _bcpModeles = new ObservableCollection<C_Client>();
+        public ObservableCollection<C_Client> bcpModeles
+        {
+            get { return _bcpModeles; }
+            set { _bcpModeles = value; }
+        }
+        #endregion
+
+        public class VM_UnModele : BasePropriete
+        {
+            private int _id, _cylindree, _puissance, _poids;
+            private string _modele, _motorisation, _carburant;
+            private float _consommation;
+
+            public int id
+            {
+                get { return _id; }
+                set { AssignerChamp<int>(ref _id, value, System.Reflection.MethodBase.GetCurrentMethod().Name); }
+            }
+            public string modele
+            {
+                get { return _modele; }
+                set { AssignerChamp<string>(ref _modele, value, System.Reflection.MethodBase.GetCurrentMethod().Name); }
+            }
+            public string motorisation
+            {
+                get { return _motorisation; }
+                set { AssignerChamp<string>(ref _motorisation, value, System.Reflection.MethodBase.GetCurrentMethod().Name); }
+            }
+            public string carburant
+            {
+                get { return _carburant; }
+                set { AssignerChamp<string>(ref _carburant, value, System.Reflection.MethodBase.GetCurrentMethod().Name); }
+            }
+
+            public int cylindree
+            {
+                get { return _cylindree; }
+                set { AssignerChamp<int>(ref _cylindree, value, System.Reflection.MethodBase.GetCurrentMethod().Name); }
+            }
+
+            public int puissance
+            {
+                get { return _puissance; }
+                set { AssignerChamp<int>(ref _puissance, value, System.Reflection.MethodBase.GetCurrentMethod().Name); }
+            }
+
+            public float consommation
+            {
+                get { return _consommation; }
+                set { AssignerChamp<float>(ref _consommation, value, System.Reflection.MethodBase.GetCurrentMethod().Name); }
+            }
+
+            public int poids
+            {
+                get { return _poids; }
+                set { AssignerChamp<int>(ref _poids, value, System.Reflection.MethodBase.GetCurrentMethod().Name); }
+            }
+
+        }
     }
 }
