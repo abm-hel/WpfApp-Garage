27694075e817c399b31275055b3319c540bd8daa
Adil BEN MOUSSA <adil-benmoussa@hotmail.fr>
2021-12-27 11:08:10 +0100

    Ajout du view model 'Modele' Développement de la patie Données écran

---
+++ ViewModel/Modele.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using Projet_Garage.Classes;
+using Projet_Garage.Gestion;
+using System.Configuration;
+
+namespace WpfApp_Garage.ViewModel
+{
+    class VM_Modele : BasePropriete
+    {
+
+        #region Données Écran
+        private string chaineConnexion = ConfigurationManager.ConnectionStrings["WpfApp_Garage.Properties.Settings.chaineConnexionBD"].ConnectionString;
+
+        private int nAjout;
+        private bool _ActiverUneFiche;
+        public bool ActiverUneFiche
+        {
+            get { return _ActiverUneFiche; }
+            set
+            {
+                AssignerChamp<bool>(ref _ActiverUneFiche, value, System.Reflection.MethodBase.GetCurrentMethod().Name);
+                ActiverBcpFiche = !ActiverUneFiche;
+            }
+        }
+
+        private bool _ActiverBcpFiche;
+        public bool ActiverBcpFiche
+        {
+            get { return _ActiverBcpFiche; }
+            set { AssignerChamp<bool>(ref _ActiverBcpFiche, value, System.Reflection.MethodBase.GetCurrentMethod().Name); }
+        }
+
+        private C_Modele _modeleSelectionnee;
+        public C_Modele modeleSelectionnee
+        {
+            get { return _modeleSelectionnee; }
+            set { AssignerChamp<C_Modele>(ref _modeleSelectionnee, value, System.Reflection.MethodBase.GetCurrentMethod().Name); }
+        }
+        #endregion
+    }
+}
