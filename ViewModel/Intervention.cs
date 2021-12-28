caa61452c4cc5f5855737498e734348c5e0e2113
Adil BEN MOUSSA <adil-benmoussa@hotmail.fr>
2021-12-28 03:53:25 +0100

    Développement de la partie Données ecran - écran Interventions

---
+++ ViewModel/Intervention.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Projet_Garage.Classes;
+using Projet_Garage.Gestion;
+using System.Configuration;
+using System.Collections.ObjectModel;
+
+
+namespace WpfApp_Garage.ViewModel
+{
+    class VM_Intervention : BasePropriete
+    {
+        #region Données Écran
+
+        private string chaineConnexion = ConfigurationManager.ConnectionStrings["WpfApp_Garage.Properties.Settings.chaineConnexionBD"].ConnectionString;
+
+        private int nAjout;
+        private bool _ActiverUneFiche;
+        public bool ActiverUneFiche
+        {
+            get { return _ActiverUneFiche; }
+            set
+            {
+                AssignerChamp<bool>(ref _ActiverUneFiche, value, System.Reflection.MethodBase.GetCurrentMethod().Name);
+                ActiverBcpFiche = !ActiverUneFiche;
+            }
+        }
+
+        private bool _ActiverBcpFiche;
+        public bool ActiverBcpFiche
+        {
+            get { return _ActiverBcpFiche; }
+            set { AssignerChamp<bool>(ref _ActiverBcpFiche, value, System.Reflection.MethodBase.GetCurrentMethod().Name); }
+        }
+
+        private C_Intervention _interventionSelectionnee;
+        public C_Intervention interventionSelectionnee
+        {
+            get { return _interventionSelectionnee; }
+            set { AssignerChamp<C_Intervention>(ref _interventionSelectionnee, value, System.Reflection.MethodBase.GetCurrentMethod().Name); }
+        }
+        #endregion
+    }
+}
