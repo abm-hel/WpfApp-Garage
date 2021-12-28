86c58ecb9ede03e47cf3846fa5518fccdf0f4de4
Adil BEN MOUSSA <adil-benmoussa@hotmail.fr>
2021-12-28 02:16:19 +0100

    Finalisation de l'interface 'Pièces'

--- View/Pieces.xaml.cs
+++ View/Pieces.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.IO;
+using Projet_Garage.Classes;
 
 namespace WpfApp_Garage.View
 {
@@ -19,14 +21,36 @@ namespace WpfApp_Garage.View
     /// </summary>
     public partial class Pieces : Window
     {
+        private ViewModel.VM_Piece localPiece;
         public Pieces()
         {
             InitializeComponent();
+            localPiece = new ViewModel.VM_Piece();
+            DataContext = localPiece;
+            FlowDocument fd = new FlowDocument();
+            Paragraph p = new Paragraph();
+            p.Inlines.Add(new Bold(new Run("Titre de document")));
+            p.Inlines.Add(new LineBreak());
+            p.Inlines.Add(new Run("Liste des pièces encodées"));
+            fd.Blocks.Add(p);
+            List l = new List();
+            foreach (C_Piece cp in localPiece.bcpPieces)
+            {
+                Paragraph pl = new Paragraph(new Run(cp.nom + " (" + cp.quantite
+                  + " pièce(s) restante(s) )"));
+                l.ListItems.Add(new ListItem(pl));
+            }
+            fd.Blocks.Add(l);
+            richTextBoxDoc.Document = fd;
+            FileStream fs = new FileStream(@"pieces.rtf", FileMode.Create);
+            TextRange tr = new TextRange(richTextBoxDoc.Document.ContentStart, richTextBoxDoc.Document.ContentEnd);
+            tr.Save(fs, System.Windows.DataFormats.Rtf);
         }
 
         private void dataGridPieces_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            if (dataGridPieces.SelectedIndex >= 0)
+                localPiece.PieceSelectionnee2UnePiece();
         }
     }
 }
