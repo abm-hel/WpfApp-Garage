3e4ffa2f6f4e2342ccddfc9cde89ad6ab7341230
Adil BEN MOUSSA <adil-benmoussa@hotmail.fr>
2022-01-05 19:28:58 +0100

    Création du view model entretiens

---
+++ ViewModel/Entretien.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Projet_Garage.Classes;
+using Projet_Garage.Gestion;
+using System.Configuration;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace WpfApp_Garage.ViewModel
+{
+    class VM_Entretien : BasePropriete
+    {
+
+    }
+}
