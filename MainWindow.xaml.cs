51039cdc2fbef76c26a17a47f6579991d240f7cd
Adil BEN MOUSSA <adil-benmoussa@hotmail.fr>
2022-01-23 22:38:06 +0100

    Ajout d'une liste déroulante pour la sélection d'entretien 'Facture et Fiche d'entrée'

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -134,109 +134,125 @@ namespace WpfApp_Garage
 
         private void buttonGenererFacture_Click(object sender, RoutedEventArgs e)
         {
-            C_Entretien entretien = new G_Entretien(chaineConnexion).Lire_ID(Convert.ToInt32(textBoxEntretien.Text));
-            C_Vehicule vehicule = new G_Vehicule(chaineConnexion).Lire_ID(Convert.ToInt32(entretien.vehiculeId));
-            List<C_Entretien_Intervention> interventions = new G_Entretien_Intervention(chaineConnexion).Lire("id");
-            List<C_Entretien_Piece> pieces = new G_Entretien_Piece(chaineConnexion).Lire("id");
-            FlowDocument fdFacture = new FlowDocument();
-            Paragraph p = new Paragraph();
-
-            p.Inlines.Add(new Bold(new Run("Facture " + entretien.id + " - " + Convert.ToDateTime(entretien.datePassage).ToString("dd/MM/yyyy").ToString())));
-            p.Inlines.Add(new LineBreak());
-            p.Inlines.Add(new LineBreak());
-            p.Inlines.Add(new Bold(new Run("Interventions")));
-            p.Inlines.Add(new LineBreak());
-            p.Inlines.Add(new LineBreak());
-
-
-
-            foreach (C_Entretien_Intervention intervention in interventions)
+            if (comboBoxEntretien.SelectedValue == null)
             {
-                if (intervention.entretienId == Convert.ToInt32(textBoxEntretien.Text))
-                {
-                    C_Intervention i = new G_Intervention(chaineConnexion).Lire_ID(Convert.ToInt32(intervention.interventionId));
+                MessageBox.Show("Veuillez choisir un entretien !");
+            }
 
-                    p.Inlines.Add(i.description);
-                    p.Inlines.Add(new LineBreak());
-                    p.Inlines.Add(string.Format("{0:0.00}", intervention.prixHeure) + " € / h" + "\t" + i.nombreHeures + " heure(s)" + "\t" + string.Format("{0:0.0}", intervention.tva) + " % TVA" + "\t" + string.Format("{0:0.00}", intervention.prix+(intervention.prix*intervention.tva/100) + " € TTC"));
-                    p.Inlines.Add(new LineBreak());
-                    p.Inlines.Add(new LineBreak());
+            else
+            {
+                C_Entretien entretien = new G_Entretien(chaineConnexion).Lire_ID(Convert.ToInt32(comboBoxEntretien.SelectedValue));
+                C_Vehicule vehicule = new G_Vehicule(chaineConnexion).Lire_ID(Convert.ToInt32(entretien.vehiculeId));
+                List<C_Entretien_Intervention> interventions = new G_Entretien_Intervention(chaineConnexion).Lire("id");
+                List<C_Entretien_Piece> pieces = new G_Entretien_Piece(chaineConnexion).Lire("id");
+                FlowDocument fdFacture = new FlowDocument();
+                Paragraph p = new Paragraph();
+
+                p.Inlines.Add(new Bold(new Run("Facture " + entretien.id + " - " + Convert.ToDateTime(entretien.datePassage).ToString("dd/MM/yyyy").ToString())));
+                p.Inlines.Add(new LineBreak());
+                p.Inlines.Add(new LineBreak());
+                p.Inlines.Add(new Bold(new Run("Interventions")));
+                p.Inlines.Add(new LineBreak());
+                p.Inlines.Add(new LineBreak());
+
+
+
+                foreach (C_Entretien_Intervention intervention in interventions)
+                {
+                    if (intervention.entretienId == Convert.ToInt32(comboBoxEntretien.SelectedValue))
+                    {
+                        C_Intervention i = new G_Intervention(chaineConnexion).Lire_ID(Convert.ToInt32(intervention.interventionId));
+
+                        p.Inlines.Add(i.description);
+                        p.Inlines.Add(new LineBreak());
+                        p.Inlines.Add(string.Format("{0:0.00}", intervention.prixHeure) + " € / h" + "\t" + i.nombreHeures + " heure(s)" + "\t" + string.Format("{0:0.0}", intervention.tva) + " % TVA" + "\t" + string.Format("{0:0.00}", intervention.prix + (intervention.prix * intervention.tva / 100) + " € TTC"));
+                        p.Inlines.Add(new LineBreak());
+                        p.Inlines.Add(new LineBreak());
+                    }
                 }
+
+                p.Inlines.Add(new Bold(new Run("Pièces")));
+                p.Inlines.Add(new LineBreak());
+                p.Inlines.Add(new LineBreak());
+
+                foreach (C_Entretien_Piece piece in pieces)
+                {
+                    if (piece.entretienId == Convert.ToInt32(comboBoxEntretien.SelectedValue))
+                    {
+                        C_Piece pi = new G_Piece(chaineConnexion).Lire_ID(Convert.ToInt32(piece.pieceId));
+                        p.Inlines.Add(pi.fabricant + " " + pi.nom);
+                        p.Inlines.Add(new LineBreak());
+                        p.Inlines.Add(string.Format("{0:0.00}", pi.prix) + " € / pièce" + "\t" + " x " + piece.quantite + "\t" + string.Format("{0:0.0}", piece.tva) + " % TVA" + "\t" + string.Format("{0:0.00}", piece.prix * piece.quantite + (piece.prix * piece.quantite * piece.tva / 100)) + " € TTC");
+                        p.Inlines.Add(new LineBreak());
+                        p.Inlines.Add(new LineBreak());
+
+
+                    }
+                }
+
+                fdFacture.Blocks.Add(p);
+                richTextBoxFacture.Document = fdFacture;
+                FileStream fs = new FileStream(@"Facture" + entretien.id + ".rtf", FileMode.Create);
+                TextRange tr = new TextRange(richTextBoxFacture.Document.ContentStart, richTextBoxFacture.Document.ContentEnd);
+                tr.Save(fs, System.Windows.DataFormats.Rtf);
+                MessageBox.Show("Facture créée !");
             }
             
-            p.Inlines.Add(new Bold(new Run("Pièces")));
-            p.Inlines.Add(new LineBreak());
-            p.Inlines.Add(new LineBreak());
-
-            foreach (C_Entretien_Piece piece in pieces)
-            {
-                if (piece.entretienId == Convert.ToInt32(textBoxEntretien.Text))
-                {
-                    C_Piece pi = new G_Piece(chaineConnexion).Lire_ID(Convert.ToInt32(piece.pieceId));
-                    p.Inlines.Add(pi.fabricant+ " "+pi.nom);
-                    p.Inlines.Add(new LineBreak());
-                    p.Inlines.Add(string.Format("{0:0.00}", pi.prix) + " € / pièce" + "\t" +" x "+ piece.quantite + "\t" + string.Format("{0:0.0}", piece.tva) + " % TVA" + "\t" + string.Format("{0:0.00}", piece.prix*piece.quantite+((piece.prix*piece.quantite)*piece.tva/100)) + " € TTC");
-                    p.Inlines.Add(new LineBreak());
-                    p.Inlines.Add(new LineBreak());
-
-
-                }
-            }
-
-            fdFacture.Blocks.Add(p);
-            richTextBoxFacture.Document = fdFacture;
-            FileStream fs = new FileStream(@"Facture" + entretien.id + ".rtf", FileMode.Create);
-            TextRange tr = new TextRange(richTextBoxFacture.Document.ContentStart, richTextBoxFacture.Document.ContentEnd);
-            tr.Save(fs, System.Windows.DataFormats.Rtf);
-            MessageBox.Show("Facture créée !");
 
         }
 
         private void buttonGenererFicheEntreeVehicule_Click(object sender, RoutedEventArgs e)
         {
-            C_Entretien entretien = new G_Entretien(chaineConnexion).Lire_ID(Convert.ToInt32(textBoxEntretien.Text));
-            C_Vehicule vehicule = new G_Vehicule(chaineConnexion).Lire_ID(Convert.ToInt32(entretien.vehiculeId));
-            List<C_Entretien_Intervention> interventions = new G_Entretien_Intervention(chaineConnexion).Lire("id");
-            List<C_Entretien_Piece> pieces = new G_Entretien_Piece(chaineConnexion).Lire("id");
-            FlowDocument fdFicheEntree = new FlowDocument();
-            Paragraph p = new Paragraph();
-            p.Inlines.Add(new Bold(new Run("Fiche d'entrée pour le véhicule "+vehicule.immatriculation+"("+Convert.ToDateTime(entretien.datePassage).ToString("dd/MM/yyyy") +")".ToString()))); 
-            p.Inlines.Add(new LineBreak());
-            p.Inlines.Add(new LineBreak());
-            p.Inlines.Add(new Bold(new Run("Interventions à effectuer")));
-            
-            foreach (C_Entretien_Intervention intervention in interventions)
+            if (comboBoxEntretien.SelectedValue == null)
             {
-                if (intervention.entretienId == Convert.ToInt32(textBoxEntretien.Text))
-                {
-                    C_Intervention i = new G_Intervention(chaineConnexion).Lire_ID(Convert.ToInt32(intervention.interventionId));
-                    
-                    p.Inlines.Add(new LineBreak());
-                    p.Inlines.Add(i.description);
-                }
-            }
-            p.Inlines.Add(new LineBreak());
-            p.Inlines.Add(new LineBreak());
-            p.Inlines.Add(new Bold(new Run("Pièces nécessaires")));
-          
-            foreach (C_Entretien_Piece piece in pieces)
-            {
-                if (piece.entretienId == Convert.ToInt32(textBoxEntretien.Text))
-                {
-                    C_Piece pi = new G_Piece(chaineConnexion).Lire_ID(Convert.ToInt32(piece.pieceId));
-                    
-                    p.Inlines.Add(new LineBreak());
-                    p.Inlines.Add(pi.nom);
-                }
+                MessageBox.Show("Veuillez choisir un entretien !");
             }
 
-            fdFicheEntree.Blocks.Add(p);
-            richTextBoxFiche.Document = fdFicheEntree;
-            FileStream fs = new FileStream(@"Fiche"+entretien.id+".rtf", FileMode.Create);
-            TextRange tr = new TextRange(richTextBoxFiche.Document.ContentStart, richTextBoxFiche.Document.ContentEnd);
-            tr.Save(fs, System.Windows.DataFormats.Rtf);
-            MessageBox.Show("Fichier créé !");
+            else
+            {
+                C_Entretien entretien = new G_Entretien(chaineConnexion).Lire_ID(Convert.ToInt32(comboBoxEntretien.SelectedValue));
+                C_Vehicule vehicule = new G_Vehicule(chaineConnexion).Lire_ID(Convert.ToInt32(entretien.vehiculeId));
+                List<C_Entretien_Intervention> interventions = new G_Entretien_Intervention(chaineConnexion).Lire("id");
+                List<C_Entretien_Piece> pieces = new G_Entretien_Piece(chaineConnexion).Lire("id");
+                FlowDocument fdFicheEntree = new FlowDocument();
+                Paragraph p = new Paragraph();
+                p.Inlines.Add(new Bold(new Run("Fiche d'entrée pour le véhicule " + vehicule.immatriculation + "(" + Convert.ToDateTime(entretien.datePassage).ToString("dd/MM/yyyy") + ")".ToString())));
+                p.Inlines.Add(new LineBreak());
+                p.Inlines.Add(new LineBreak());
+                p.Inlines.Add(new Bold(new Run("Interventions à effectuer")));
 
+                foreach (C_Entretien_Intervention intervention in interventions)
+                {
+                    if (intervention.entretienId == Convert.ToInt32(comboBoxEntretien.SelectedValue))
+                    {
+                        C_Intervention i = new G_Intervention(chaineConnexion).Lire_ID(Convert.ToInt32(intervention.interventionId));
+
+                        p.Inlines.Add(new LineBreak());
+                        p.Inlines.Add(i.description);
+                    }
+                }
+                p.Inlines.Add(new LineBreak());
+                p.Inlines.Add(new LineBreak());
+                p.Inlines.Add(new Bold(new Run("Pièces nécessaires")));
+
+                foreach (C_Entretien_Piece piece in pieces)
+                {
+                    if (piece.entretienId == Convert.ToInt32(comboBoxEntretien.SelectedValue))
+                    {
+                        C_Piece pi = new G_Piece(chaineConnexion).Lire_ID(Convert.ToInt32(piece.pieceId));
+
+                        p.Inlines.Add(new LineBreak());
+                        p.Inlines.Add(pi.nom+"\tx"+piece.quantite);
+                    }
+                }
+
+                fdFicheEntree.Blocks.Add(p);
+                richTextBoxFiche.Document = fdFicheEntree;
+                FileStream fs = new FileStream(@"Fiche" + entretien.id + ".rtf", FileMode.Create);
+                TextRange tr = new TextRange(richTextBoxFiche.Document.ContentStart, richTextBoxFiche.Document.ContentEnd);
+                tr.Save(fs, System.Windows.DataFormats.Rtf);
+                MessageBox.Show("Fiche d'entrée créée !");
+            }
         }
     }
 }
